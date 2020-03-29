5ecd03830009cda39054574333672434188a6d32
Nhan Nguyen <nkmnhan@gmail.com>
2020-03-29 16:50:46 +0700

    Add submodule and login page

---
+++ Server/IdentityServer4.Sample/InfrastmentStructure/ExternalProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentityServer4.Sample.InfrastmentStructure
+{
+    public class ExternalProvider
+    {
+        public string DisplayName { get; set; }
+        public string AuthenticationScheme { get; set; }
+    }
+}
---
+++ Server/IdentityServer4.Sample/ViewModels/RedirectViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace IdentityServer4.Sample.ViewModels
+{
+    public class RedirectViewModel
+    {
+        public string RedirectUrl { get; set; }
+    }
+}
